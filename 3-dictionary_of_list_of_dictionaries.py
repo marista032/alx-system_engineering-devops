94da096d1d9dfaa70c862ec05ecace79e68abce2
marista032 <marista032@gmail.com>
2023-04-25 15:52:12 -0700

    commit for task 3 - Dictionary of list of dictionaries

--- 3-dictionary_of_list_of_dictionaries.py
+++ 3-dictionary_of_list_of_dictionaries.py
@@ -1,21 +1,18 @@
-2	Antonette	False	suscipit repellat esse quibusdam voluptatem incidunt
-2	Antonette	True	distinctio vitae autem nihil ut molestias quo
-2	Antonette	False	et itaque necessitatibus maxime molestiae qui quas velit
-2	Antonette	False	adipisci non ad dicta qui amet quaerat doloribus ea
-2	Antonette	True	voluptas quo tenetur perspiciatis explicabo natus
-2	Antonette	True	aliquam aut quasi
-2	Antonette	True	veritatis pariatur delectus
-2	Antonette	False	nesciunt totam sit blanditiis sit
-2	Antonette	False	laborum aut in quam
-2	Antonette	True	nemo perspiciatis repellat ut dolor libero commodi blanditiis omnis
-2	Antonette	False	repudiandae totam in est sint facere fuga
-2	Antonette	False	earum doloribus ea doloremque quis
-2	Antonette	False	sint sit aut vero
-2	Antonette	False	porro aut necessitatibus eaque distinctio
-2	Antonette	True	repellendus veritatis molestias dicta incidunt
-2	Antonette	True	excepturi deleniti adipisci voluptatem et neque optio illum ad
-2	Antonette	False	sunt cum tempora
-2	Antonette	False	totam quia non
-2	Antonette	False	doloremque quibusdam asperiores libero corrupti illum qui omnis
-2	Antonette	True	totam atque quo nesciunt
+#!/usr/bin/python3
+"""Script is to export all the employees' information to JSON format."""
+import json
+import requests
 
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    users = requests.get(url + "users").json()
+
+    with open("todo_all_employees.json", "w") as jsonfile:
+        json.dump({
+            u.get("id"): [{
+                "task": t.get("title"),
+                "completed": t.get("completed"),
+                "username": u.get("username")
+            } for t in requests.get(url + "todos",
+                                    params={"userId": u.get("id")}).json()]
+            for u in users}, jsonfile)
